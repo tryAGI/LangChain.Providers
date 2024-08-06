717571a98921967bce1f314e2f176bfc31dead2b
HavenDV <havendv@gmail.com>
2024-08-06 16:03:48 +0400

    test: Added IntegrationTests.

---
+++ src/IntegrationTests/ProviderType.cs
@@ -0,0 +1,16 @@
+namespace LangChain.IntegrationTests;
+
+public enum ProviderType
+{
+    OpenAi,
+    Together,
+    Anyscale,
+    Fireworks,
+    OpenRouter,
+    DeepInfra,
+    Google,
+    Anthropic,
+    Groq,
+    DeepSeek,
+    Azure,
+}
