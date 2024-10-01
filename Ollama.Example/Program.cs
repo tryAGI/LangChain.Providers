098aaec5751272ded7c726444fe0a202966ca28d
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2024-10-01 20:55:24 +0300

    Added an examples solution

---
+++ Ollama.Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using LangChain.Chains.LLM;
+using LangChain.Prompts;
+using LangChain.Providers;
+using LangChain.Providers.Ollama;
+using LangChain.Schema;
+
+namespace Ollama.Example;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        OllamaChatModel model = new OllamaChatModel(new OllamaProvider(), "llama3.1");
+        PromptTemplate template = new PromptTemplate(new PromptTemplateInput(
+            "You are an AI model that creates poems in pirate speak , here is the subject of the poem:<content>{content}</content>",
+            ["content"]));
+        Dictionary<string, object> chainValueDict = new();
+
+        ChainValues chainValues = new(chainValueDict);
+        LlmChain chain = new LlmChain(new LlmChainInput(model, template));
+
+        model.UseConsoleForDebug();
+
+        Console.WriteLine("type exit to exit");
+        Console.WriteLine("Enter the subject of the poem:");
+        
+        chainValueDict["content"] = "";
+
+        while (chainValueDict["content"].ToString().Trim() != "exit")
+        {
+            chainValueDict["content"] = Console.ReadLine();
+            var response = await chain.CallAsync(chainValues);
+            Console.WriteLine(response.Value["text"].ToString());
+        }
+    }
+}
