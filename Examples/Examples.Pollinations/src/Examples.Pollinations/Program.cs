0ae13dfdcb279e7ba1ca5e533fe5a9eec41f9a45
AnonymousBit <68566858+AnonymousBit0111@users.noreply.github.com>
2024-10-06 19:37:24 +0300

    feat: Created an example for Pollinations AI API

---
+++ Examples/Examples.Pollinations/src/Examples.Pollinations/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LangChain.Providers;
+using LangChain.Providers.Pollinations;
+
+
+PollinationsConfiguration config = new()
+{
+    ApiKey = "None"
+};
+PollinationsProvider provider = new(config);
+
+PollinationsModel  model = new(provider,"mistral");
+
+var response = await model.GenerateAsync("What is your name");
+
+Console.WriteLine($"Response from mistral was : {response.LastMessageContent}\n");
