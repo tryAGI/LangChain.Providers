d184cfc8b5533075e232a7d6ad81543bc909a355
danijerez <danieljerezgarrido@gmail.com>
2024-10-21 16:23:15 +0200

    add to providers

--- src/OpenAI/src/OpenAiProvider.cs
+++ src/OpenAI/src/OpenAiProvider.cs
@@ -1,4 +1,5 @@
 using OpenAI;
+using System.Reflection;
 
 namespace LangChain.Providers.OpenAI;
 
@@ -74,4 +75,18 @@ public class OpenAiProvider : Provider
     {
         return new OpenAiProvider(apiKey);
     }
+
+    public static OpenAiProvider ToGroqProvider(string apiKey) => new(CustomProviders.Groq(apiKey)) { Id = nameof(CustomProviders.Groq) };
+    public static OpenAiProvider ToDeepInfraProvider(string apiKey) => new(CustomProviders.DeepInfra(apiKey)) { Id = nameof(CustomProviders.DeepInfra) };
+    public static OpenAiProvider ToDeepSeekProvider(string apiKey) => new(CustomProviders.DeepSeek(apiKey)) { Id = nameof(CustomProviders.DeepSeek) };
+    public static OpenAiProvider ToFireworksProvider(string apiKey) => new(CustomProviders.Fireworks(apiKey)) { Id = nameof(CustomProviders.Fireworks) };
+    public static OpenAiProvider ToOpenRouterProvider(string apiKey) => new(CustomProviders.OpenRouter(apiKey)) { Id = nameof(CustomProviders.OpenRouter) };
+    public static OpenAiProvider ToTogetherProvider(string apiKey) => new(CustomProviders.Together(apiKey)) { Id = nameof(CustomProviders.Together) };
+    public static OpenAiProvider ToPerplexityProvider(string apiKey) => new(CustomProviders.Perplexity(apiKey)) { Id = nameof(CustomProviders.Perplexity) };
+    public static OpenAiProvider ToSambaNovaProvider(string apiKey) => new(CustomProviders.SambaNova(apiKey)) { Id = nameof(CustomProviders.SambaNova) };
+    public static OpenAiProvider ToMistralProvider(string apiKey) => new(CustomProviders.Mistral(apiKey)) { Id = nameof(CustomProviders.Mistral) };
+    public static OpenAiProvider ToCodestralProvider(string apiKey) => new(CustomProviders.Codestral(apiKey)) { Id = nameof(CustomProviders.Codestral) };
+    public static OpenAiProvider ToOllamaProvider(Uri? baseUri = null) => new(CustomProviders.Ollama(baseUri)) { Id = nameof(CustomProviders.Ollama) };
+    public static OpenAiProvider ToLmStudioProvider(Uri? baseUri = null) => new(CustomProviders.LmStudio(baseUri)) { Id = nameof(CustomProviders.LmStudio) };
+
 }
